4d8cd80c3a499fbe144642a657fb2f70e20e4391
Antonio Di Gennaro <digennaroantonio@yahoo.it>
2015-01-23 00:12:17 +0100

    Added comments with example of usage

--- cachematrix.R
+++ cachematrix.R
@@ -24,6 +24,31 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve takes as input a list object generated with makeCacheMatrix
 ## If the Inverse Matrix is found cached, it's returned otherwise is calculated
 ## using the Solve function , cached for future reference and finally returned.
+##
+## Example 
+##
+## > a <- matrix (c(1,3,3,1), nrow=2)
+## > a
+## [,1] [,2]
+## [1,]    1    3
+## [2,]    3    1
+## > a_cache <- makeCacheMatrix(a)
+## > cacheSolve(a_cache)
+## [,1]   [,2]
+## [1,] -0.125  0.375
+## [2,]  0.375 -0.125
+## > cacheSolve(a_cache)
+## getting cached data
+## [,1]   [,2]
+## [1,] -0.125  0.375
+## [2,]  0.375 -0.125
+## >
+## > a %*% cacheSolve(a_cache)
+## getting cached data
+## [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
+## > 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
